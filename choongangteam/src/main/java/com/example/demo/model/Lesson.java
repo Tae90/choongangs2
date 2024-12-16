a14f45922958b0a22241b30fe6b3a55e40a4eacc
manbo <ziastar@naver.com>
2024-12-16 15:23:51 +0900

    paymentdetail 결제 관련 수정

--- choongangteam/src/main/java/com/example/demo/model/Lesson.java
+++ choongangteam/src/main/java/com/example/demo/model/Lesson.java
@@ -23,6 +23,7 @@ public class Lesson {
 	private String lesson_keyword;
 	private int lesson_price;
 	private int lesson_apply;
+	private int lesson_currentapply;
 
 	private int reply_count;
 	private int favorite_count;
